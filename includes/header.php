fc0d606e0f211c61e5f6bc975c50fcc419e8bd08
Emilie Ostheimer <emilie93.ostheimer@gmail.com>
2020-01-06 11:29:34 +0100

    maj global

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<header>
+  <h1>Le site </h1>
+  <nav>
+    <ul>
+      <li  class="menu-acc">
+        <a href="index.php?page=accueil" title="Page d'accueil">Accueil</a></li>
+      <li  class="menu-pre">
+        <a href="index.php?page=presentation" title="Presentation">Presentation</a></li>
+      <li  class="menu-cv">
+        <a href="index.php?page=cv" title="CV">Mon CV</a></li>
+      <li  class="menu-con">
+        <a href="index.php?page=contact" title="Nous contacter">Contact</a></li>
+      <li  class="menu-men">
+        <a href="index.php?page=mentions" title="Les mentions légales">Mentions légales</a></li>
+    </ul>
+  </nav>
+</header>
---
+++ includes/html.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bienvenue</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+  </head>
+  <body>
